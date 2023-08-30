0b5cc32af36b165f55391b52abe770f9b3d08225
Sakshi Ware <126942483+sakshiware25@users.noreply.github.com>
2023-08-30 21:23:13 +0530

    Add files via upload
    WEEK 1 Problem Set

---
+++ hello.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<cs50.h>
+
+int main(void)
+
+{
+
+    string name = get_string("whats your name?\n");                //get user input
+
+    printf("Hello,%s\n",name);            //print hello,user input
+
+
+
+}
+
